0f3650f180fe4d9a340b9c0d13a52819a035b387
Thomas Hammons <thomas.hammons_jr@uconn.edu>
2016-02-02 14:11:01 -0500

    Create jesusChrist.php
    Jesus could be real 
    Or he could not be 
    I honestly have no idea.

---
+++ jesusChrist.php
@@ -0,0 +1,49 @@
+<!-- TOM YOU HAVE TO CHANGE THE CSS DIRECTORY -->
+<html>
+	<head>
+		<title> idk </title>
+		<link rel = "stylesheet" type = "text/css" href = "./?.css" />
+		<script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"> </script>
+
+
+		
+		
+	</head>
+
+
+
+	<body>
+		<?php
+			
+			
+
+
+
+		?>
+
+		<script language = "JavaScript">
+		<!--
+			
+
+
+
+		//-->
+		</script>
+
+	</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
